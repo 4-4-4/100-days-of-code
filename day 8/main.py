584bdab120a6b0a68a6721713c195108b1c0d081
yassin <yassinelsarraf@gmail.com>
2023-05-09 19:46:39 -0400

    day 8 Caeser cipher
    Caeser cipher, function compressed and added user experience improvements

--- day 8/main.py
+++ day 8/main.py
@@ -1,24 +1,26 @@
+import art
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-
 
 def caeser(text,shift,direction):
     cipher =""
-    if direction == "encode":
-        for n in text:
+    if direction == "decode":
+        shift *=-1
+    for n in text:
+        if n not in alphabet:
+            cipher+=n
+        else:
             i= alphabet.index(n)
             cipher+=alphabet[i+shift] 
-        print("the encoded text is "+cipher)
-    elif direction == "decode":
-        shift += 26
-        for n in text:
-            i= alphabet.index(n)
-            cipher+=alphabet[i-shift]
-        print("the encoded text is "+cipher)  
-    else:
-        print("wrong input") 
-caeser(text,shift,direction)
+    print(f"the {direction}d text is {cipher}.")
+
+print(art.logo)
+repeat ="yes"
+while repeat == "yes":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift %26
+    caeser(text,shift,direction)
+    repeat = input("would you like to restart? yes/no\n")
+else:
+    print("goodbye ^_^")

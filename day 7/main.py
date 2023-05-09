58566f523504eb00796b2cf2b97122fcbd660ae5
yassin <yassinelsarraf@gmail.com>
2023-05-09 17:14:18 -0400

    day 7 hangman
    final product

--- day 7/main.py
+++ day 7/main.py
@@ -1,84 +1,47 @@
-'''#Step 1 
+
+import hangman_art
+import hangman_words
 import random
-word_list = ["aardvark", "baboon", "camel"]
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-word = random.choice(word_list)
-
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("guess the letter: ")
-guess.lower()
-
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-n=0
-while n <= len(word):
-    print(guess in word[n-1])
-    n+=1
-'''
-'''
-#Step 2
-
-import random
-word_list = ["aardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
-
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
-
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-display=[]
-for letter in chosen_word:
-    display+="_"
-    
-guess = input("Guess a letter: ").lower()
-
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-n=1
-while n <= len(word):
-    if guess in word[n-1]:
-        display[n-1]= '"'+guess+'"'
-    n+=1
+from hangman_words import word_list
+from hangman_art import logo, stages
 
 
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-print(display) '''
 
-#Step 3
-
-import random
-word_list = ["aardvark", "baboon", "camel"]
 word = random.choice(word_list)
 word_length = len(word)
+end_of_game = False
 
-#Testing code
 print(f'Pssst, the solution is {word}.')
 
-#Create blanks
 display = []
 for _ in range(word_length):
     display += "_"
 
-#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
-while "_" in display:
-    guess = input("Guess a letter: ").lower()
-    n=1
-    while n <= len(word):
-        if guess in word[n-1]:
-            display[n-1]= '"'+guess+'"'
-        n+=1
-    print (display)
-else:
-    print("you won")
-#Check guessed letter
-for position in range(word_length):
-    letter = word[position]
-    print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
-    if letter == guess:
-        display[position] = letter
+lives = 6
 
-print(display)
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    n = 1
+    if guess in display:
+        print("you already found this letter")
+    for position in range(word_length):
+        letter = word[position]
+        
+        
+        if letter == guess:
+            display[position] = letter
+    if guess not in word:
+        print("wrong answer bud")
+        lives = lives -1
+        if lives == 0:
+            print("no lives remaining u lose lol")
+            end_of_game= True
+    print (stages[lives])     
+    print(f"{' '.join(display)}")
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+    
+        
+       
+    
